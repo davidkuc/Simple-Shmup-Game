dac2a241c7a85ead683fc87f2270f37b7290ba58
davidkuc <91789757+davidkuc@users.noreply.github.com>
2023-01-25 20:05:01 +0100

    Removed commented code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //[SerializeField] float moveDistanceLimit = 0.3f;
     [SerializeField] int maxRowIndex = 4;
     int currentIndex = 2;
     [SerializeField] float stepDistance = 0.1f;
@@ -13,22 +12,6 @@ public class PlayerMovement : MonoBehaviour
     {
         startingPoint = transform.position;
     }
-
-    //public void MoveUp()
-    //{
-    //    if (WillExceedDistanceLimit(stepDistance))
-    //        return;
-
-    //    transform.position += new Vector3(0, stepDistance, 0);
-    //}
-
-    //public void MoveDown()
-    //{
-    //    if (WillExceedDistanceLimit(-stepDistance))
-    //        return;
-
-    //    transform.position += new Vector3(0, -stepDistance, 0);
-    //}
 
     public void MoveUp()
     {
@@ -55,9 +38,4 @@ public class PlayerMovement : MonoBehaviour
     {
         return up ? currentIndex + 1 > maxRowIndex : currentIndex - 1 < 0;
     }
-
-    //private bool WillExceedDistanceLimit(float step)
-    //{
-    //    return  Math.Abs(transform.position.y) + step > moveDistanceLimit;
-    //}
 }
