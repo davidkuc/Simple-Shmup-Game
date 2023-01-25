fa3340bf259fc9b8b50d08c3b69c87077a1e3f68
davidkuc <91789757+davidkuc@users.noreply.github.com>
2023-01-25 18:05:33 +0100

    Added logic for saving and loading

--- Assets/Scenes/Scripts/DataManager.cs
+++ Assets/Scenes/Scripts/DataManager.cs
@@ -1,13 +1,64 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
     public event Action DataUpdated;
 
-    PlayerData_SO playerData_SO;
+    SaveSystem saveSystem;
 
-    public PlayerData_SO PlayerData_SO => playerData_SO; 
+    PlayerData_SO playerData_SO;
+    PlayerDataStructure playerDataStructure;
+
+    public PlayerData_SO PlayerData_SO => playerData_SO;
+
+    private void Awake()
+    {
+        saveSystem = new SaveSystem();
+    }
+
+    public void SaveData()
+    {
+        if (playerDataStructure == null)
+        {
+            Debug.Log("Creating new player data structure on SaveData()");
+            playerDataStructure = CreateNewPlayerDataStructure();
+        }
+
+        MapPlayer_SO_ToPlayerDataStructure();
+        saveSystem.SavePlayer(playerDataStructure);
+    }
+
+    public void LoadData()
+    {
+        if (playerDataStructure == null)
+        {
+            Debug.Log("Creating new player data structure on LoadData()");
+            playerDataStructure = CreateNewPlayerDataStructure();
+        }
+
+        playerDataStructure = saveSystem.LoadPlayer();
+        MapPlayerDataStructureToPlayer_SO();
+    }
+
+    private void MapPlayer_SO_ToPlayerDataStructure()
+    {
+        playerDataStructure.BestScore = playerData_SO.BestScore;
+        playerDataStructure.LatestScore = playerData_SO.LatestScore;
+    }
+
+    private void MapPlayerDataStructureToPlayer_SO()
+    {
+        playerData_SO.BestScore = playerDataStructure.BestScore;
+        playerData_SO.LatestScore = playerDataStructure.LatestScore;
+    }
+
+    private PlayerDataStructure CreateNewPlayerDataStructure()
+    {
+        var newPlayerData = new PlayerDataStructure();
+        newPlayerData.BestScore = playerData_SO.BestScore;
+        newPlayerData.LatestScore = playerData_SO.LatestScore;
+
+        return newPlayerData;
+    }
 }
