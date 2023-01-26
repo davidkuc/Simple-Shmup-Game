f1aa1a7374ecaf0929910ef386c62b716d132ef3
davidkuc <91789757+davidkuc@users.noreply.github.com>
2023-01-26 11:31:04 +0100

    Removed UI Manager, now the logic is present in the UI elements themselves

--- Assets/Scripts/UI/UI_GameScreen.cs
+++ Assets/Scripts/UI/UI_GameScreen.cs
@@ -1,7 +1,34 @@
+using Zenject;
+
 public class UI_GameScreen : UI_Container
 {
+    GameManager gameManager;
+
+    [Inject]
+    public void Setup(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
     private void OnEnable()
     {
-        
+        gameManager.GameRunStarted += EnableCanvas;
+        gameManager.GameRunEnded += DisableCanvas;
+    }
+
+    private void OnDisable()
+    {
+        gameManager.GameRunStarted -= EnableCanvas;
+        gameManager.GameRunEnded -= DisableCanvas;
+    }
+
+    private void EnableCanvas()
+    {
+        ToggleCanvas(true);
+    }
+
+    private void DisableCanvas()
+    {
+        ToggleCanvas(false);
     }
 }
--- Assets/Scripts/UI/UI_MainScreen.cs
+++ Assets/Scripts/UI/UI_MainScreen.cs
@@ -1,4 +1,35 @@
+using System;
+using Zenject;
+
 public class UI_MainScreen : UI_Container
 {
+    GameManager gameManager;
 
+    [Inject]
+    public void Setup(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
+    private void OnEnable()
+    {
+        gameManager.GameRunStarted += DisableCanvas;
+        gameManager.GameRunEnded += EnableCanvas;
+    }
+
+    private void OnDisable()
+    {
+        gameManager.GameRunStarted -= DisableCanvas;
+        gameManager.GameRunEnded -= EnableCanvas;
+    }
+
+    private void EnableCanvas()
+    {
+        ToggleCanvas(true);
+    }
+
+    private void DisableCanvas()
+    {
+        ToggleCanvas(false);
+    }
 }
